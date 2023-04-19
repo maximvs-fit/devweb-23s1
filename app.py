3aecccb81c9e5896b38eff54f7c62026a0734d2c
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2023-04-18 22:34:46 -0300

    separa o get e o post do form em funções diferentes

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/cadastro', methods=['GET', 'POST'])
-def register():
-    if request.method == 'GET':
-        print('processando requisição GET')
+@app.route('/cadastro')
+def get_register():
+    print('processando requisição GET')
+    return render_template('form.html')
 
-    if request.method == 'POST':
-        print('processando requisição POST')
 
-    pprint(vars(request))
-
+@app.route('/cadastro', methods=['POST'])
+def post_register():
+    print('processando requisição POST')
+    pprint(request.form)
     return render_template('form.html')
 
 
